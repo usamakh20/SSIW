1. added requirements.txt 2. added zero shot classification for CMP Facade dataset 3. added legend with predictions 4. added accuracy calculation 5. minor fixes

Test_Minist/tools/test.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-import os, sys
-CODE_SPACE=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import os
+import sys
+
+CODE_SPACE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(CODE_SPACE)
 os.chdir(CODE_SPACE)
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import argparse
 import cv2
@@ -16,23 +18,24 @@ import torch
 import torch.nn.functional as F
 import json
 
-
 import utils.config as config
 from utils.config import CfgNode
 from utils.transforms_utils import get_imagenet_mean_std, normalize_img, pad_to_crop_sz, resize_by_scaled_short_side
-import matplotlib.pyplot as plt
-from utils.color_seg import color_seg
+from utils.color_seg import color_seg, PALETTE, add_legend, calculate_accuracy
 
 import glob
-from PIL import Image
-from utils.labels_dict import UNI_UID2UNAME, ALL_LABEL2ID, UNAME2EM_NAME
+from utils.labels_dict import UNI_UID2UNAME
 import torch.multiprocessing as mp
 from utils.segformer import get_configured_segformer
 from tqdm import tqdm
 import torch.distributed as dist
+import torchvision.transforms as transforms
 
 from utils.get_class_emb import create_embs_from_names
 import ast
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+transform = transforms.ToTensor()
 
 
 def get_logger():
@@ -48,6 +51,7 @@ def get_logger():
         logger.addHandler(handler)
     return logger
 
+
 logger = get_logger()
 
 
@@ -56,55 +60,47 @@ def get_parser() -> CfgNode:
     TODO: add to library to avoid replication.
     """
     parser = argparse.ArgumentParser(description='Yvan Yin\'s Semantic Segmentation Model.')
-    parser.add_argument('--root_dir', type=str, help='root dir for the data')
-    parser.add_argument('--cam_id', type=str, help='camera ID')
+    parser.add_argument('--root_dir', type=str, default='test_imgs/default', help='root dir for the data')
+    parser.add_argument('--cam_id', type=str, default='01', help='camera ID')
     parser.add_argument('--img_folder', default='image_', type=str, help='the images folder name except the camera ID')
-    parser.add_argument('--img_file_type', default='jpeg', type=str, help='the file type of images, such as jpeg, png, jpg...')
+    parser.add_argument('--img_file_type', default='png', type=str,
+                        help='the file type of images, such as jpeg, png, jpg...')
 
-    parser.add_argument('--config', type=str, default='720_ss', help='config file')
+    parser.add_argument('--config', type=str, default='test_720_ss', help='config file')
     parser.add_argument('--gpus_num', type=int, default=1, help='number of gpus')
     parser.add_argument('--save_folder', type=str, default='ann/semantics', help='the folder for saving semantic masks')
 
-    parser.add_argument('--user_label', nargs='*', help='the label user identified for semantic segmentation')
+    parser.add_argument('--user_label', nargs='*', default=[],
+                        help='the label user identified for semantic segmentation')
     parser.add_argument('--new_definitions', type=ast.literal_eval, help='new label definitions identified by user')
-    parser.add_argument('opts', help='see mseg_semantic/config/test/default_config_360.yaml for all options, models path should be passed in',
-        default=None, nargs=argparse.REMAINDER)
+    parser.add_argument('opts',
+                        help='see mseg_semantic/config/test/default_config_360.yaml for all options, models path should be passed in',
+                        default=None, nargs=argparse.REMAINDER)
 
     args = parser.parse_args()
     config_path = os.path.join('configs', f'{args.config}.yaml')
-    args.config = config_path
+    cfg = config.load_cfg_from_cfg_file(config_path)
 
     # test on samples
-    if args.root_dir is None:
-        args.root_dir = f'{CODE_SPACE}/test_imgs'
-        args.cam_id='01'
-        args.img_file_type = 'png'
+    cfg.root_dir = f'{CODE_SPACE}/{cfg.get("root_dir", default=args.root_dir)}'
 
-
-    if args.user_label:
-        args.user_label = [i for i in args.user_label]
-
-    assert args.config is not None
-    cfg = config.load_cfg_from_cfg_file(args.config)
-    cfg.root_dir = args.root_dir
     cfg.cam_id = args.cam_id
     cfg.img_folder = args.img_folder
-    cfg.img_file_type = args.img_file_type
-    cfg.gpus_num = args.gpus_num
-    cfg.save_folder = args.save_folder
+    cfg.new_definitions = cfg.get('new_definitions', default=args.new_definitions)
+    cfg.img_file_type = cfg.get('img_file_type', default=args.img_file_type)
+    cfg.gpus_num = cfg.get('gpus_num', default=args.gpus_num)
+    cfg.save_folder = cfg.get('save_folder', default=args.save_folder)
+    cfg.user_label = cfg.get('user_label', default=args.user_label)
 
-    cfg.user_label = args.user_label
-    cfg.new_definitions = args.new_definitions
     return cfg
 
 
-  
 def get_prediction(embs, gt_embs_list):
     prediction = []
     logits = []
     B, _, _, _ = embs.shape
     for b in range(B):
-        score = embs[b,...]
+        score = embs[b, ...]
         score = score.unsqueeze(0)
         emb = gt_embs_list
         emb = emb / emb.norm(dim=1, keepdim=True)
@@ -122,20 +118,20 @@ def get_prediction(embs, gt_embs_list):
         logit = torch.cat(logits, dim=0)
     return logit
 
-  
+
 def single_scale_single_crop_cuda(model,
-                      image: np.ndarray,
-                      h: int, w: int, gt_embs_list,
-                      args=None) -> np.ndarray:
+                                  image: np.ndarray,
+                                  h: int, w: int, gt_embs_list,
+                                  args=None) -> np.ndarray:
     ori_h, ori_w, _ = image.shape
     mean, std = get_imagenet_mean_std()
     crop_h = (np.ceil((ori_h - 1) / 32) * 32).astype(np.int32)
     crop_w = (np.ceil((ori_w - 1) / 32) * 32).astype(np.int32)
-    
+
     image, pad_h_half, pad_w_half = pad_to_crop_sz(image, crop_h, crop_w, mean)
     image_crop = torch.from_numpy(image.transpose((2, 0, 1))).float()
     normalize_img(image_crop, mean, std)
-    image_crop = image_crop.unsqueeze(0).cuda()
+    image_crop = image_crop.unsqueeze(0).to(DEVICE)
     with torch.no_grad():
         emb, _, _ = model(inputs=image_crop, label_space=['universal'])
         logit = get_prediction(emb, gt_embs_list)
@@ -152,10 +148,10 @@ def single_scale_single_crop_cuda(model,
     prediction = cv2.resize(prediction_crop, (w, h), interpolation=cv2.INTER_LINEAR)
     return prediction
 
-  
+
 def single_scale_cuda(model,
                       image: np.ndarray,
-                      h: int, w: int, gt_embs_list, stride_rate: float = 2/3,
+                      h: int, w: int, gt_embs_list, stride_rate: float = 2 / 3,
                       args=None) -> np.ndarray:
     mean, std = get_imagenet_mean_std()
     crop_h = args.test_h
@@ -163,13 +159,13 @@ def single_scale_cuda(model,
     ori_h, ori_w, _ = image.shape
     image, pad_h_half, pad_w_half = pad_to_crop_sz(image, crop_h, crop_w, mean)
     new_h, new_w, _ = image.shape
-    stride_h = int(np.ceil(crop_h*stride_rate))
-    stride_w = int(np.ceil(crop_w*stride_rate))
-    grid_h = int(np.ceil(float(new_h-crop_h)/stride_h) + 1)
-    grid_w = int(np.ceil(float(new_w-crop_w)/stride_w) + 1)
+    stride_h = int(np.ceil(crop_h * stride_rate))
+    stride_w = int(np.ceil(crop_w * stride_rate))
+    grid_h = int(np.ceil(float(new_h - crop_h) / stride_h) + 1)
+    grid_w = int(np.ceil(float(new_w - crop_w) / stride_w) + 1)
 
-    prediction_crop = torch.zeros((gt_embs_list.shape[0], new_h, new_w)).cuda()
-    count_crop = torch.zeros((new_h, new_w)).cuda()
+    prediction_crop = torch.zeros((gt_embs_list.shape[0], new_h, new_w)).to(DEVICE)
+    count_crop = torch.zeros((new_h, new_w)).to(DEVICE)
     # loop w/ sliding window, obtain start/end indices
     for index_h in range(0, grid_h):
         for index_w in range(0, grid_w):
@@ -193,27 +189,28 @@ def single_scale_cuda(model,
 
     prediction_crop /= count_crop.unsqueeze(0)
     # disregard predictions from padded portion of image
-    prediction_crop = prediction_crop[:, pad_h_half:pad_h_half+ori_h, pad_w_half:pad_w_half+ori_w]
+    prediction_crop = prediction_crop[:, pad_h_half:pad_h_half + ori_h, pad_w_half:pad_w_half + ori_w]
 
     # CHW -> HWC
-    prediction_crop = prediction_crop.permute(1,2,0)
+    prediction_crop = prediction_crop.permute(1, 2, 0)
     prediction_crop = prediction_crop.data.cpu().numpy()
 
     # upsample or shrink predictions back down to scale=1.0
     prediction = cv2.resize(prediction_crop, (w, h), interpolation=cv2.INTER_LINEAR)
     return prediction
 
-  
+
 def do_test(args, local_rank):
     imgs_on_devices = organize_images(args, local_rank)
     model = get_configured_segformer(args.num_model_classes,
                                      criterion=None,
-                                     load_imagenet_model=False)
+                                     load_imagenet_model=False,
+                                     batch_norm_gpu=bool(args.gpus_num))
     model.eval()
 
     if args.distributed:
-        model = torch.nn.parallel.DistributedDataParallel(model.cuda(),
-                                                          device_ids=[local_rank,],
+        model = torch.nn.parallel.DistributedDataParallel(model.to(DEVICE),
+                                                          device_ids=[local_rank, ],
                                                           output_device=local_rank,
                                                           find_unused_parameters=True)
     else:
@@ -224,7 +221,6 @@ def do_test(args, local_rank):
     ckpt_filter = {k: v for k, v in checkpoint.items() if 'criterion.0.criterion.weight' not in k}
     model.load_state_dict(ckpt_filter, strict=False)
 
-
     if args.user_label:
         if args.new_definitions:
             gt_embs_list = create_embs_from_names(args.user_label, args.new_definitions).float()
@@ -233,7 +229,7 @@ def do_test(args, local_rank):
         args.id_to_label = {i: v for i, v in enumerate(args.user_label)}
         args.id_to_label[255] = 'unlabel'
     else:
-        gt_embs_list = torch.tensor(np.load(args.emb_path)).cuda().float()
+        gt_embs_list = torch.tensor(np.load(args.emb_path)).to(DEVICE).float()
         args.id_to_label = UNI_UID2UNAME
         if args.new_definitions:
             new_labels = [label for label in args.new_definitions.keys()]
@@ -242,19 +238,19 @@ def do_test(args, local_rank):
             gt_embs_list = torch.cat([gt_embs_list, new_embs], dim=0)
             nums = len(args.id_to_label)
             for i in range(len(new_labels)):
-                args.id_to_label[nums+i-1] = new_labels[i]
-
+                args.id_to_label[nums + i - 1] = new_labels[i]
 
     test_single(args, imgs_on_devices, local_rank, model, gt_embs_list)
 
 
 def test_single(args, imgs_list, local_rank, model, gt_embs_list):
+    accuracies = []
     for i, rgb_path in tqdm(enumerate(imgs_list)):
         save_path = os.path.join(args.root_dir, args.save_folder, os.path.basename(rgb_path))
         save_path = os.path.splitext(save_path)[0] + '.png'
-        
+
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
-        
+
         rgb = cv2.imread(rgb_path, -1)[:, :, ::-1]
         image_resized = resize_by_scaled_short_side(rgb, args.base_size, 1)
         h, w, _ = rgb.shape
@@ -263,19 +259,17 @@ def test_single(args, imgs_list, local_rank, model, gt_embs_list):
             out_logit = single_scale_single_crop_cuda(model, image_resized, h, w, gt_embs_list=gt_embs_list, args=args)
         elif args.single_scale_multi_crop:
             out_logit = single_scale_cuda(model, image_resized, h, w, gt_embs_list=gt_embs_list, args=args)
-                                      
-        prediction = out_logit.argmax(axis=-1).squeeze()   # (h, w)
+
+        prediction = out_logit.argmax(axis=-1).squeeze()  # (h, w)
         probs = out_logit.max(axis=-1).squeeze()  # (h, w)
         high_prob_mask = probs > 0.5
-        
+
         mask = high_prob_mask
         prediction[~mask] = 255
 
         labels = np.unique(prediction)
         label_names = {i: args.id_to_label[i] for i in labels}
         # label_names = [id_to_label[i] for i in labels]
-        print()
-        print(f'label_names for img{i+1}', label_names)
 
         # # change to initial color
         # if not args.new_definitions:
@@ -286,67 +280,96 @@ def test_single(args, imgs_list, local_rank, model, gt_embs_list):
         #             mask = (prediction == label)
         #             prediction[mask] = UNAME.index(args.user_label[label])
 
-        pred_color = color_seg(prediction)
-        vis_seg = visual_segments(pred_color, rgb)
 
-        vis_seg.save(os.path.splitext(save_path)[0] + '_vis.png')
         cv2.imwrite(save_path, prediction.astype(np.uint8))
-        
-def visual_segments(segments, rgb):
-    seg = Image.fromarray(segments)
-    rgb = Image.fromarray(rgb)
+        cv2.imwrite(os.path.splitext(save_path)[0] + '_vis.png',
+                    cv2.cvtColor(visual_segments(prediction, rgb, label_names), cv2.COLOR_BGR2RGB))
 
-    seg1 = seg.convert('RGBA')
-    rgb1 = rgb.convert('RGBA')
+        if args.get('calculate_accuracy',default=False):
+            current_accuracy = calculate_accuracy(rgb_path.split('.')[0] + '.xml', prediction)
+            print(f'accuracy for img{i + 1}', current_accuracy)
+            accuracies.append(current_accuracy)
 
-    vis_seg = Image.blend(rgb1, seg1, 0.8)
-    return vis_seg
-  
+    if accuracies:
+        overall_accuracy = np.mean(accuracies)
+        print(overall_accuracy)
+        with open(os.path.join(args.root_dir, args.save_folder, 'overall_accuracy.txt'),'w') as f:
+            f.write(str(overall_accuracy))
+
+
+def visual_segments(prediction, rgb, label_names):
+    pred_color = color_seg(prediction)
+    seg = cv2.cvtColor(pred_color, cv2.COLOR_BGR2BGRA)
+    rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2BGRA)
+    result = cv2.cvtColor(cv2.addWeighted(rgb, 0.7, seg, 0.3, 0.0), cv2.COLOR_BGRA2BGR)
+
+    return add_legend(result, [(PALETTE[i].tolist(), label) for i, label in label_names.items()])
+
+
 def organize_images(args, local_rank):
     imgs_dir = args.root_dir
     imgs_list = glob.glob(imgs_dir + f'/*.{args.img_file_type}')
     imgs_list.sort()
-    num_devices = args.gpus_num
+    num_devices = args.gpus_num or 1
 
     imgs_on_device = imgs_list[local_rank::num_devices]
     return imgs_on_device
+
 
 def main_worker(local_rank: int, cfg: dict):
     if cfg.distributed:
         global_rank = local_rank
         world_size = cfg.gpus_num
-        
-        torch.cuda.set_device(global_rank)
+
+        torch.cuda.set_device(DEVICE)
         dist.init_process_group(backend="nccl",
-                            init_method=cfg.dist_url,
-                            world_size=world_size,
-                            rank=global_rank,)
+                                init_method=cfg.dist_url,
+                                world_size=world_size,
+                                rank=global_rank, )
     do_test(cfg, local_rank)
+
+
+def mask_centers(mask: np.ndarray, label_mapping: dict) -> dict:
+    """
+    Compute the centers of masks by calculating their bounding boxes
+    """
+    result = {}
+
+    for label in label_mapping:
+        if label != 255:
+            row, col = np.where(mask == label)
+            if row.size > 0 and col.size > 0:
+                result[label] = (int(np.mean(col)), int(np.mean(row)))
+                # result[label] = [np.min(row), np.min(col), np.max(row), np.max(col)]
+
+    return result
+
 
 if __name__ == '__main__':
 
     args = get_parser()
     logger.info(args)
-    
+
     dist_url = 'tcp://127.0.0.1:6769'
     dist_url = dist_url[:-2] + str(os.getpid() % 100).zfill(2)
     args.dist_url = dist_url
-    
+
     num_gpus = torch.cuda.device_count()
     if num_gpus != args.gpus_num:
-        raise RuntimeError('The set gpus number cannot match the detected gpus number. Please check or set CUDA_VISIBLE_DEVICES')
-    
+        raise RuntimeError(
+            'The set gpus number cannot match the detected gpus number. Please check or set CUDA_VISIBLE_DEVICES')
+
     if num_gpus > 1:
         args.distributed = True
     else:
         args.distributed = False
-    
+
     save_path = os.path.join(args.root_dir, args.save_folder, 'id2labels.json')
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     with open(save_path, 'w') as f:
         json.dump(UNI_UID2UNAME, f)
-    
+
     if not args.distributed:
         main_worker(0, args)
     else:
-        mp.spawn(main_worker, nprocs=args.gpus_num, args=(args, ))
+        mp.spawn(main_worker, nprocs=args.gpus_num, args=(args,))

Test_Minist/utils/color_seg.py
@@ -1,4 +1,17 @@
+import xml.etree.ElementTree as ET
 import numpy as np
+import cv2
+
+# Define the size of the rectangle
+rectangle_size = 0.1
+
+# Create a blank image to draw the legend on
+legend_height = 700
+legend_width = 300
+
+font_scale = 0.8
+thickness = 1
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 
 def make_palette(num_classes=256):
@@ -25,6 +38,7 @@ def make_palette(num_classes=256):
     palette[num_classes - 1, :] = [255, 255, 255]
     return palette
 
+
 PALETTE = make_palette(256)
 
 
@@ -35,8 +49,9 @@ def color_seg(seg, palette=None):
         color_out = palette[seg.reshape(-1)].reshape(seg.shape + (3,))
     return color_out
 
+
 def color_map_list(class_num):
-    map1 =  np.asarray([
+    map1 = np.asarray([
         [0, 0, 0],
         [120, 120, 120],
         [180, 120, 120],
@@ -185,48 +200,48 @@ def color_map_list(class_num):
         [25, 194, 194],
         [102, 255, 0],
         [92, 0, 255],
-      [165, 42, 42],
-      [0, 192, 0],
-      [196, 196, 196],
-      [190, 153, 153],
-      [180, 165, 180],
-      [102, 102, 156],
-      [128, 64, 255],
-      [140, 140, 200],
-      [170, 170, 170],
-      [250, 170, 160],
-      [96, 96, 96],
-      [230, 150, 140],
-      [128, 64, 128],
-      [110, 110, 110],
-      [244, 35, 232],
-      [150, 100, 100],
-      [70, 70, 70],
-      [150, 120, 90],
-      [220, 20, 60],
-      [255, 0, 0],
-      [200, 128, 128],
-      [64, 170, 64],
-      [128, 64, 64],
-      [70, 130, 180],
-      [152, 251, 152],
-      [107, 142, 35],
-      [0, 170, 30],
-      [255, 255, 128],
-      [250, 0, 30],
-      [220, 220, 220],
-      [222, 40, 40],
-      [100, 170, 30],
-      [40, 40, 40],
-      [33, 33, 33],
-      [0, 0, 142],
-      [210, 170, 100],
-      [153, 153, 153],
-      [128, 128, 128],
-      [250, 170, 30],
-      [192, 192, 192],
-      [220, 220, 0],
-      [119, 11, 32],
+        [165, 42, 42],
+        [0, 192, 0],
+        [196, 196, 196],
+        [190, 153, 153],
+        [180, 165, 180],
+        [102, 102, 156],
+        [128, 64, 255],
+        [140, 140, 200],
+        [170, 170, 170],
+        [250, 170, 160],
+        [96, 96, 96],
+        [230, 150, 140],
+        [128, 64, 128],
+        [110, 110, 110],
+        [244, 35, 232],
+        [150, 100, 100],
+        [70, 70, 70],
+        [150, 120, 90],
+        [220, 20, 60],
+        [255, 0, 0],
+        [200, 128, 128],
+        [64, 170, 64],
+        [128, 64, 64],
+        [70, 130, 180],
+        [152, 251, 152],
+        [107, 142, 35],
+        [0, 170, 30],
+        [255, 255, 128],
+        [250, 0, 30],
+        [220, 220, 220],
+        [222, 40, 40],
+        [100, 170, 30],
+        [40, 40, 40],
+        [33, 33, 33],
+        [0, 0, 142],
+        [210, 170, 100],
+        [153, 153, 153],
+        [128, 128, 128],
+        [250, 170, 30],
+        [192, 192, 192],
+        [220, 220, 0],
+        [119, 11, 32],
         [0, 80, 100],
         [149, 32, 32],
         [10, 59, 140],
@@ -242,3 +257,61 @@ def color_map_list(class_num):
     pa = np.ones((class_num, 3), dtype=np.uint8) * 255
     pa[:map1.shape[0], :] = map1
     return pa
+
+
+def add_legend(image, entries):
+    legend = np.zeros((legend_height, legend_width, 3), dtype=np.uint8)
+    # Loop through the colors and draw rectangles and labels
+    for i, (color, label) in enumerate(entries):
+        # Calculate the position of the rectangle
+        x1 = int(legend_width * 0.05)
+        y1 = int(i * 50 + 0.05 * legend_height)
+        x2 = int(legend_width * (0.05 + rectangle_size))
+        y2 = int((i + 1) * 50 - 0.05 * legend_height)
+        # Draw the rectangle
+        cv2.rectangle(legend, (x1, y1), (x2, y2), color, -1)
+
+        # Draw the label
+        text_size = cv2.getTextSize(label, font, font_scale, thickness)[0]
+        text_x = int(legend_width * (0.05 + rectangle_size + 0.05))
+        text_y = int(y1 + (y2 - y1 + text_size[1]) / 2)
+        cv2.putText(legend, label, (text_x, text_y), font, font_scale, color, thickness, cv2.LINE_AA)
+
+    # Find the largest height between the two images
+    max_height = max(image.shape[0], legend.shape[0])
+
+    # Determine the amount of padding required for each image
+    padding1 = (max_height - image.shape[0]) // 2
+    padding2 = (max_height - legend.shape[0]) // 2
+
+    # Create a blank image with the largest height and the combined width
+    combined_width = image.shape[1] + legend.shape[1]
+    result = np.zeros((max_height, combined_width, 3), dtype=np.uint8)
+
+    # Paste the first image into the blank image with padding
+    result[padding1:padding1 + image.shape[0], :image.shape[1]] = image
+
+    # Paste the second image into the blank image with padding
+    result[padding2:padding2 + legend.shape[0], image.shape[1]:] = legend
+
+    return result
+
+
+def calculate_accuracy(xml_file, predicted):
+    with open(xml_file) as f:
+        data = '<root>' + f.read() + '</root>'
+
+    predicted[predicted == 255] = 0
+    root = ET.fromstring(data)
+    mask = np.zeros(predicted.shape, dtype=np.uint8)
+    for obj in root.findall('object'):
+        label = int(obj.find('label').text)
+        points = obj.find('points')
+        x = [int(float(pt.text) * mask.shape[0]) - 1 for pt in points.findall('x')]
+        y = [int(float(pt.text) * mask.shape[1]) - 1 for pt in points.findall('y')]
+        cv2.rectangle(mask, (y[0], x[0]), (y[1], x[1]), label, -1)
+
+    correct = np.equal(predicted, mask).sum()
+    accuracy = correct / (mask.shape[0]*mask.shape[1])
+
+    return accuracy

Test_Minist/utils/decode_heads/segformer_head.py
@@ -28,7 +28,7 @@ class SegFormerHead(BaseDecodeHead):
     """
     SegFormer: Simple and Efficient Design for Semantic Segmentation with Transformers
     """
-    def __init__(self, feature_strides, **kwargs):
+    def __init__(self, feature_strides, norm_cfg, **kwargs):
         super(SegFormerHead, self).__init__(input_transform='multiple_select', **kwargs)
         assert len(feature_strides) == len(self.in_channels)
         assert min(feature_strides) == feature_strides[0]
@@ -48,7 +48,7 @@ class SegFormerHead(BaseDecodeHead):
             in_channels=embedding_dim*4,
             out_channels=embedding_dim,
             kernel_size=1,
-            norm_cfg=dict(type='SyncBN', requires_grad=True)
+            norm_cfg=norm_cfg
         )
 
         self.linear_pred = nn.Conv2d(embedding_dim, self.num_classes, kernel_size=1)

Test_Minist/utils/get_class_emb.py
@@ -1,14 +1,14 @@
-import torch
-import numpy as np
 import os
-import os.path as osp
-from utils.labels_dict import UNI_UID2UNAME, ALL_LABEL2ID, UNAME2EM_NAME
+import torch
+from sentence_transformers import SentenceTransformer
+from utils.labels_dict import UNI_UID2UNAME, UNAME2EM_NAME
+
 UNI_UNAME2ID = {v: i for i, v in UNI_UID2UNAME.items()}
 
+
 def create_embs_from_names(labels, other_descriptions=None):
-    import clip
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    CLIP_TEXT_MODEL, PREPROCESS = clip.load("ViT-B/32", device=DEVICE)
+    CLIP_TEXT_MODEL = SentenceTransformer('clip-ViT-B-32', device=DEVICE)
     u_descrip_dir = 'data/clip_descriptions'
     embs = []
     for name in labels:
@@ -18,11 +18,9 @@ def create_embs_from_names(labels, other_descriptions=None):
         elif name in other_descriptions:
             description = other_descriptions[name]
 
-        text = clip.tokenize([description, ]).to(DEVICE)
         with torch.no_grad():
-            text_features = CLIP_TEXT_MODEL.encode_text(text)
+            text_features = CLIP_TEXT_MODEL.encode([description, ], convert_to_tensor=True, device=DEVICE)
         text_features /= text_features.norm(dim=-1, keepdim=True)
         embs.append(text_features)
     embs = torch.stack(embs, dim=0).squeeze()
     return embs
-

Test_Minist/utils/segformer.py
@@ -4,13 +4,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 from functools import partial
 
-#from mmseg.models.builder import BACKBONES
+# from mmseg.models.builder import BACKBONES
 from mmseg.utils import get_root_logger
 from mmcv.runner import load_checkpoint
 from mmcv.cnn import ConvModule, DepthwiseSeparableConvModule
 
 import math
-#from .decode_heads.decode_head import BaseDecodeHead
+# from .decode_heads.decode_head import BaseDecodeHead
 from utils.decode_heads.aspp_head import ASPPHead, ASPPModule
 from mmcv.cnn.bricks import build_norm_layer
 from mmseg.ops import resize
@@ -19,7 +19,10 @@ from mmseg.ops import resize
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 import logging
 from utils.decode_heads.segformer_head import SegFormerHead
+
 logger = logging.getLogger(__name__)
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
 
 class FCNHead(nn.Module):
     """Fully Convolution Networks for Semantic Segmentation.
@@ -55,8 +58,8 @@ class FCNHead(nn.Module):
                 kernel_size=kernel_size,
                 padding=kernel_size // 2,  #
                 conv_cfg=None,
-                norm_cfg=self.norm_cfg, # sync bn
-                act_cfg=self.act_cfg)) # relu
+                norm_cfg=self.norm_cfg,  # sync bn
+                act_cfg=self.act_cfg))  # relu
 
         self.convs = nn.Sequential(*convs)
         self.cls_seg = nn.Conv2d(in_channels=self.channels, out_channels=self.num_classes,
@@ -68,6 +71,7 @@ class FCNHead(nn.Module):
         output = self.convs(x)
         output = self.cls_seg(output)
         return output
+
 
 class DepthwiseSeparableASPPModule(ASPPModule):
     """Atrous Spatial Pyramid Pooling (ASPP) Module with depthwise separable
@@ -124,7 +128,6 @@ class DynHead(nn.Module):
             nn.Conv2d(in_channels // channel_reduce_factor, num_out_channel, 1)
         )
 
-
         if zero_init:
             nn.init.constant_(self.classifier[-1].weight, 0)
         else:
@@ -138,7 +141,7 @@ class DynHead(nn.Module):
         return self.classifier(feature)
 
 
-#@HEADS.register_module()
+# @HEADS.register_module()
 class BilinearPADHead_fast_xavier_init(ASPPHead):
     """Encoder-Decoder with Atrous Separable Convolution for Semantic Image
     Segmentation.
@@ -220,20 +223,19 @@ class BilinearPADHead_fast_xavier_init(ASPPHead):
         self.register_buffer("coord", coord_tmp.float(), persistent=False)
 
     def computer_locations_per_level(self, height, width, h=8, w=8):
-        shifts_x = torch.arange(0, 1, step=1/w, dtype=torch.float32)
-        shifts_y = torch.arange(0, 1, step=1/h, dtype=torch.float32)
+        shifts_x = torch.arange(0, 1, step=1 / w, dtype=torch.float32)
+        shifts_y = torch.arange(0, 1, step=1 / h, dtype=torch.float32)
         shift_y, shift_x = torch.meshgrid(shifts_y, shifts_x)
         locations = torch.stack((shift_x, shift_y), dim=0)
         stride_h = height // 32
         stride_w = width // 32
-        coord = locations.repeat(stride_h*stride_w, 1, 1, 1)
+        coord = locations.repeat(stride_h * stride_w, 1, 1, 1)
         return coord
-
 
     def forward(self, inputs):
         """Forward function."""
         # inputs: [1/32 stage, 1/4 stage]
-        x = inputs[0] # 1/32 stage
+        x = inputs[0]  # 1/32 stage
 
         aspp_outs = [
             resize(
@@ -298,7 +300,7 @@ class BilinearPADHead_fast_xavier_init(ASPPHead):
         B, conv_ch, H, W = x.size()
         weights, biases = self.get_subnetworks_params_fast(x, channels=dy_ch)
         f = self.upsample_f
-        #self.coord_generator(H, W)
+        # self.coord_generator(H, W)
         coord = self.coord.reshape(1, H, W, 2, f, f).permute(0, 3, 1, 4, 2, 5).reshape(1, 2, H * f, W * f)
         coord = coord.repeat(B, 1, 1, 1)
         if x_cat is not None:
@@ -405,22 +407,21 @@ class BilinearPADHead_fast_xavier_init(ASPPHead):
         H = height
         W = width
         coord = coord.repeat(H * W, 1, 1, 1)
-        self.coord = coord.to(device='cuda')
+        self.coord = coord.to(device=DEVICE)
 
 
 def compute_locations_per_level(h, w):
     shifts_x = torch.arange(
         0, 1, step=1 / w,
-        dtype=torch.float32, device='cuda'
+        dtype=torch.float32, device=DEVICE
     )
     shifts_y = torch.arange(
         0, 1, step=1 / h,
-        dtype=torch.float32, device='cuda'
+        dtype=torch.float32, device=DEVICE
     )
     shift_y, shift_x = torch.meshgrid(shifts_y, shifts_x)
     locations = torch.stack((shift_x, shift_y), dim=0)
     return locations
-
 
 
 class Mlp(nn.Module):
@@ -617,13 +618,13 @@ class MixVisionTransformer(nn.Module):
 
         # patch_embed
         self.patch_embed1 = OverlapPatchEmbed(img_size=img_size, patch_size=7, stride=4, in_chans=in_chans,
-                                              embed_dim=embed_dims[0]) # 1/4
+                                              embed_dim=embed_dims[0])  # 1/4
         self.patch_embed2 = OverlapPatchEmbed(img_size=img_size // 4, patch_size=3, stride=2, in_chans=embed_dims[0],
-                                              embed_dim=embed_dims[1]) # 1/8
+                                              embed_dim=embed_dims[1])  # 1/8
         self.patch_embed3 = OverlapPatchEmbed(img_size=img_size // 8, patch_size=3, stride=2, in_chans=embed_dims[1],
-                                              embed_dim=embed_dims[2]) # auxilary output
+                                              embed_dim=embed_dims[2])  # auxilary output
         self.patch_embed4 = OverlapPatchEmbed(img_size=img_size // 16, patch_size=3, stride=2, in_chans=embed_dims[2],
-                                              embed_dim=embed_dims[3]) # 1/32
+                                              embed_dim=embed_dims[3])  # 1/32
 
         # transformer encoder
         dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]  # stochastic depth decay rule
@@ -775,8 +776,7 @@ class DWConv(nn.Module):
         return x
 
 
-
-#@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class mit_b0(MixVisionTransformer):
     def __init__(self, **kwargs):
         super(mit_b0, self).__init__(
@@ -785,7 +785,7 @@ class mit_b0(MixVisionTransformer):
             drop_rate=0.0, drop_path_rate=0.1)
 
 
-#@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class mit_b1(MixVisionTransformer):
     def __init__(self, **kwargs):
         super(mit_b1, self).__init__(
@@ -794,7 +794,7 @@ class mit_b1(MixVisionTransformer):
             drop_rate=0.0, drop_path_rate=0.1)
 
 
-#@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class mit_b2(MixVisionTransformer):
     def __init__(self, **kwargs):
         super(mit_b2, self).__init__(
@@ -803,7 +803,7 @@ class mit_b2(MixVisionTransformer):
             drop_rate=0.0, drop_path_rate=0.1)
 
 
-#@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class mit_b3(MixVisionTransformer):
     def __init__(self, **kwargs):
         super(mit_b3, self).__init__(
@@ -812,7 +812,7 @@ class mit_b3(MixVisionTransformer):
             drop_rate=0.0, drop_path_rate=0.1)
 
 
-#@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class mit_b4(MixVisionTransformer):
     def __init__(self, **kwargs):
         super(mit_b4, self).__init__(
@@ -821,7 +821,7 @@ class mit_b4(MixVisionTransformer):
             drop_rate=0.0, drop_path_rate=0.1)
 
 
-#@BACKBONES.register_module()
+# @BACKBONES.register_module()
 class mit_b5(MixVisionTransformer):
     def __init__(self, **kwargs):
         super(mit_b5, self).__init__(
@@ -829,8 +829,9 @@ class mit_b5(MixVisionTransformer):
             qkv_bias=True, norm_layer=partial(nn.LayerNorm, eps=1e-6), depths=[3, 6, 40, 3], sr_ratios=[8, 4, 2, 1],
             drop_rate=0.0, drop_path_rate=0.1)
 
+
 class SegFormer(nn.Module):
-    def __init__(self, num_classes, load_imagenet_model, imagenet_ckpt_fpath, **kwargs):
+    def __init__(self, num_classes, load_imagenet_model, imagenet_ckpt_fpath, batch_norm_gpu, **kwargs):
         super(SegFormer, self).__init__(**kwargs)
 
         self.encoder = mit_b5()
@@ -856,21 +857,22 @@ class SegFormer(nn.Module):
         self.head = SegFormerHead(num_classes=num_classes,
                                   in_channels=[64, 128, 320, 512],
                                   channels=128,
-                                  in_index=[0,1,2,3],
+                                  in_index=[0, 1, 2, 3],
                                   feature_strides=[4, 8, 16, 32],
-                                  #decoder_params=dict(embed_dim=768),
+                                  # decoder_params=dict(embed_dim=768),
                                   dropout_ratio=0.1,
-                                  norm_cfg=dict(type='SyncBN', requires_grad=True),
-                                  align_corners=False)
+                                  norm_cfg=dict(type='SyncBN' if batch_norm_gpu else 'BN', requires_grad=True),
+                                  align_corners=False,
+                                  **kwargs)
         self.auxi_net = FCNHead(num_convs=1,
                                 kernel_size=3,
                                 concat_input=True,
                                 in_channels=320,
                                 num_classes=num_classes,
-                                norm_cfg=dict(type='SyncBN', requires_grad=True))
+                                norm_cfg=dict(type='SyncBN' if batch_norm_gpu else 'BN', requires_grad=True))
         self.init_weights(load_imagenet_model, imagenet_ckpt_fpath)
 
-    def init_weights(self, load_imagenet_model: bool=False, imagenet_ckpt_fpath: str='') -> None:
+    def init_weights(self, load_imagenet_model: bool = False, imagenet_ckpt_fpath: str = '') -> None:
         """ For training, we use a models pretrained on ImageNet. Irrelevant at inference.
             Args:
             -   pretrained_fpath: str representing path to pretrained models
@@ -892,44 +894,51 @@ class SegFormer(nn.Module):
         h = inputs.size()[2]
         w = inputs.size()[3]
         x = self.encoder(inputs)
-        #out = self.head([x[3], x[0]])
+        # out = self.head([x[3], x[0]])
         out = self.head(x)
         auxi_out = self.auxi_net(x)
-        high_out = F.interpolate(out, size=(h,w), mode='bilinear', align_corners=True)
+        high_out = F.interpolate(out, size=(h, w), mode='bilinear', align_corners=True)
         return high_out, out, auxi_out
 
 
 class SegModel(nn.Module):
-    def __init__(self, criterions, num_classes, load_imagenet_model, imagenet_ckpt_fpath, **kwargs):
+    def __init__(self, criterions, num_classes, load_imagenet_model, imagenet_ckpt_fpath, batch_norm_gpu, **kwargs):
         super(SegModel, self).__init__(**kwargs)
         self.segmodel = SegFormer(num_classes=num_classes,
                                   load_imagenet_model=load_imagenet_model,
-                                  imagenet_ckpt_fpath=imagenet_ckpt_fpath)
+                                  imagenet_ckpt_fpath=imagenet_ckpt_fpath,
+                                  batch_norm_gpu=batch_norm_gpu)
         self.criterion = None
+
     def forward(self, inputs, gt=None, label_space=None, others=None):
         high_reso, low_reso, auxi_out = self.segmodel(inputs)
         return high_reso, None, None
 
+
 def get_seg_model(
-    criterion: list,
-    n_classes: int,
-    load_imagenet_model: bool = False,
-    imagenet_ckpt_fpath: str = '',
-    **kwargs
-    ) -> nn.Module:
+        criterion: list,
+        n_classes: int,
+        load_imagenet_model: bool = False,
+        imagenet_ckpt_fpath: str = '',
+        batch_norm_gpu: bool = True,
+        **kwargs
+) -> nn.Module:
     model = SegModel(criterions=criterion,
                      num_classes=n_classes,
                      load_imagenet_model=load_imagenet_model,
-                     imagenet_ckpt_fpath=imagenet_ckpt_fpath)
+                     imagenet_ckpt_fpath=imagenet_ckpt_fpath,
+                     batch_norm_gpu=batch_norm_gpu)
     assert isinstance(model, nn.Module)
     return model
 
+
 def get_configured_segformer(
-    n_classes: int,
-    criterion: list,
-    load_imagenet_model: bool = False,
-    imagenet_ckpt_fpath: str = '',
-    ) -> nn.Module:
+        n_classes: int,
+        criterion: list,
+        load_imagenet_model: bool = False,
+        imagenet_ckpt_fpath: str = '',
+        batch_norm_gpu: bool = True
+) -> nn.Module:
     """
         Args:
         -   n_classes: integer representing number of output classes
@@ -942,14 +951,14 @@ def get_configured_segformer(
                 (at training, init using imagenet-pretrained models)
     """
 
-    model = get_seg_model(criterion, n_classes, load_imagenet_model, imagenet_ckpt_fpath)
+    model = get_seg_model(criterion, n_classes, load_imagenet_model, imagenet_ckpt_fpath, batch_norm_gpu)
     return model
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     imagenet_ckpt_fpath = ''
     load_imagenet_model = False
-    criterions=[]
+    criterions = []
     from mseg_semantic.model.criterion import Cross_sim_loss
 
     loss_method = Cross_sim_loss(data_index=['universal'],

Test_Minist/utils/transforms_utils.py
@@ -4,7 +4,8 @@ import random
 import torch
 from typing import Optional, Tuple
 
-def get_imagenet_mean_std() -> Tuple[Tuple[float,float,float], Tuple[float,float,float]]:
+
+def get_imagenet_mean_std() -> Tuple[Tuple[float, float, float], Tuple[float, float, float]]:
     """ See use here in Pytorch ImageNet script: 
         https://github.com/pytorch/examples/blob/master/imagenet/main.py#L197
         Returns:
@@ -17,11 +18,11 @@ def get_imagenet_mean_std() -> Tuple[Tuple[float,float,float], Tuple[float,float
     std = [0.229, 0.224, 0.225]
     std = [item * value_scale for item in std]
     return mean, std
-            
 
-def normalize_img(  input: torch.Tensor, 
-                    mean: Tuple[float,float,float], 
-                    std: Optional[Tuple[float,float,float]] = None):
+
+def normalize_img(input: torch.Tensor,
+                  mean: Tuple[float, float, float],
+                  std: Optional[Tuple[float, float, float]] = None):
     """ Pass in by reference Torch tensor, and normalize its values.
         Args:
         -   input: Torch tensor of shape (3,M,N), must be in this order, and
@@ -38,13 +39,13 @@ def normalize_img(  input: torch.Tensor,
         for t, m, s in zip(input, mean, std):
             t.sub_(m).div_(s)
 
-            
+
 def pad_to_crop_sz(
-    image: np.ndarray,
-    crop_h: int,
-    crop_w: int,
-    mean: Tuple[float,float,float]
-    ) -> Tuple[np.ndarray,int,int]:
+        image: np.ndarray,
+        crop_h: int,
+        crop_w: int,
+        mean: Tuple[float, float, float]
+) -> Tuple[np.ndarray, int, int]:
     ori_h, ori_w, _ = image.shape
     pad_h = max(crop_h - ori_h, 0)
     pad_w = max(crop_w - ori_w, 0)
@@ -63,9 +64,9 @@ def pad_to_crop_sz(
 
 
 def resize_by_scaled_short_side(
-    image: np.ndarray,
-    base_size: int,
-    scale: float) -> np.ndarray:
+        image: np.ndarray,
+        base_size: int,
+        scale: float) -> np.ndarray:
     """ Equivalent to ResizeShort(), but functional, instead of OOP paradigm, and w/ scale param.
 
 	Args: